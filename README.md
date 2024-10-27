ac454ee857e0d41b8cedd767be21344ab8718477
headrow <31188551+headrow@users.noreply.github.com>
2024-10-27 08:31:30 +0000

    Release

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pinterest-album-fetcher
-:money:
+# Pinterest Album Fetcher
+Python script that lets you download images from Pinterest albums
+# How To Use
+- Run main.py
+- Insert your Pinterest username
+- Insert your Pinterest albums name
+- If you wish, you can insert your "_pinterest_sess" cookie or leave it empty
+- Let the script run
+# How To Get Your Pinterest Session Cookie
+- [Watch how to get it here](https://www.youtube.com/watch?v=W9lz_wQPc5E)
+# Requirements
+- [requests](https://pypi.org/project/requests/)
--- main.py
+++ main.py
@@ -1,35 +1,95 @@
 import os
 import requests
 
-def get_pins(username: str, bookmark: str = "", numOfImgs: int = 0):
-    link = "https://pt.pinterest.com/resource/UserPinsResource/get/?source_url=/" + username + "/pins/&data={\"options\":{\"is_own_profile_pins\":true,\"username\":\"" + username + "\",\"field_set_key\":\"grid_item\",\"pin_filter\":null, \"bookmarks\":[\"" + bookmark + "\"]},\"context\":{}}"
-    req = requests.get(link, timeout=5)
+def save_image(name: str, url: str, album: str):
+    folder = f"output/{username}/{album}"
+    ext = url[url.rfind("."):]
+    data = requests.get(url=url).content
+
+    if (os.path.isdir(folder) == False):
+        os.makedirs(folder)
+
+    with open(f"{folder}/{name}{ext}", "wb") as imgCopy:
+        imgCopy.write(data)
+
+    print(f"Saved the pin {name}")
+
+def get_pins(username: str, cookie: str, bookmark: str = "", numOfImgs: int = 0):
+    link = "https://www.pinterest.com/resource/UserPinsResource/get/?data={\"options\":{\"username\":\"" + username + "\", \"bookmarks\":[\"" + bookmark + "\"]}}"
+    req = requests.get(url=link, cookies={"_pinterest_sess": cookie}, timeout=5)
     response = req.json()["resource_response"]
 
     if (req.status_code == 200):
-        if (os.path.isdir(f"output/{username}/pins") == False):
-            os.makedirs(f"output/{username}/pins")
-
         for pin in response["data"]:
+            if ("node_id" not in pin):
+                continue
+
             imgName = pin["id"]
-            imgUrl = pin["images"]["orig"]["url"]
-            imgExt = imgUrl[imgUrl.rfind("."):]
-            imgData = requests.get(imgUrl).content
+            imgAlbum = pin["board"]["name"]
 
-            print(f"Saving the pin {imgName}")
-            print(f"File extension is {imgExt}")
-
-            with open(f"output/{username}/pins/{imgName}{imgExt}", "wb") as imgCopy:
-                imgCopy.write(imgData)
-
+            if ("videos" in pin) and (pin["videos"] != None):
+                imgUrl = pin["videos"]["video_list"]["V_720P"]["url"]
+            else:
+                imgUrl = pin["images"]["orig"]["url"]
+            
+            save_image(name=imgName, url=imgUrl, album=imgAlbum)
             numOfImgs += 1
         
-        if (response["bookmark"] != ""):
-            get_pins(username=username, bookmark=response["bookmark"], numOfImgs=numOfImgs)
+        if ("bookmark" in response):
+            get_pins(username=username, cookie=cookie, bookmark=response["bookmark"], numOfImgs=numOfImgs)
         else:
             print(f"Fetched {numOfImgs} images.")
     else:
-        print(f"There was a {req.status_code} error with your search: {response["error"]["message"]}")
+        print(f"There was a {req.status_code} error with your search: {response["error"]["message"]} ({response["error"]["code"]})")
 
-username = input("Insert your Pinterest username: ")
-get_pins(username=username)
+def get_album(id: str, cookie: str, bookmark: str = "", numOfImgs: int = 0):
+    link = "https://www.pinterest.com/resource/BoardFeedResource/get/?data={\"options\":{\"board_id\":\"" + id + "\", \"bookmarks\":[\"" + bookmark + "\"]}}"
+    req = requests.get(url=link, cookies={"_pinterest_sess": cookie}, timeout=5)
+    response = req.json()["resource_response"]
+
+    if (req.status_code == 200):
+        for pin in response["data"]:
+            if ("node_id" not in pin):
+                continue
+
+            imgName = pin["id"]
+            imgAlbum = pin["board"]["name"]
+
+            if ("videos" in pin) and (pin["videos"] != None):
+                imgUrl = pin["videos"]["video_list"]["V_720P"]["url"]
+            else:
+                imgUrl = pin["images"]["orig"]["url"]
+            
+            save_image(name=imgName, url=imgUrl, album=imgAlbum)
+            numOfImgs += 1
+        
+        if ("bookmark" in response):
+            get_album(id=id, cookie=cookie, bookmark=response["bookmark"], numOfImgs=numOfImgs)
+        else:
+            print(f"Fetched {numOfImgs} images.")
+    else:
+        print(f"There was a {req.status_code} error with your search: {response["error"]["message"]} ({response["error"]["code"]})")
+
+def find_album(username: str, album: str, cookie: str):
+    link = "https://www.pinterest.com/resource/BoardsResource/get/?data={\"options\":{\"username\":\"" + username + "\"}}"
+    req = requests.get(url=link, cookies={"_pinterest_sess": cookie}, timeout=5)
+    response = req.json()["resource_response"]
+
+    if (req.status_code == 200):
+        for alb in response["data"]:
+            if (alb["name"].lower() == album):
+                get_album(id=alb["id"], cookie=cookie)
+                return
+
+        print("Album wasn't found")
+    else:
+        print(f"There was a {req.status_code} error with your search: {response["error"]["message"]} ({response["error"]["code"]})")
+
+username = input("Insert your Pinterest username: ").lower()
+album = input("Insert your Pinterest albums name: ").lower()
+cookie = input("Insert your \"_pinterest_sess\" cookie (Can be left empty): ")
+
+if (album == "pins"):
+    get_pins(username=username, cookie=cookie)
+else:
+    find_album(username=username, album=album, cookie=cookie)
